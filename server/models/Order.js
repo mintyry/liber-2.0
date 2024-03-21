efe41c852d2cbf80585475356d9a94f2ed87ed68
mintyry <gitmintyry@gmail.com>
2024-03-21 15:51:12 -0700

    Order model

---
+++ server/models/Order.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const orderSchema = new Schema(
+    {
+        purchaseDate:
+        {
+            type: Date,
+            default: Date.now
+        },
+        donation:
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Donation'
+        }
+    }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+
+module.exports = Order;
